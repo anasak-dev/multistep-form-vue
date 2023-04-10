ee4b0bea9384b09b9b8380681b94371d69f46ee8
anasak-dev <anasniazi94@gmail.com>
2023-04-10 10:01:10 +0500

    added backup files in gitignore

--- src/components/clicked.js
+++
@@ -1,3 +0,0 @@
-export const clicked = (e) => {
-  console.log(e);
-};
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue/dist/vue.esm-bundler";
-import "./index.css";
-import App from "./App.vue";
-const app = createApp(App);
-app.mount("#app");
